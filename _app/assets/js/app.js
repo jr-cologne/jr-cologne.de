a0298dff2b4e169a50d79ce5af0b416009ecd2a9
JR Cologne <habichvergessen40@gmail.com>
2017-08-02 13:07:10 +0200

    Improve browser compatibility of Nav, Change Implementation of Parallax Effect in Welcome Section, Remove fixed header

--- _app/assets/js/app.js
+++ _app/assets/js/app.js
@@ -4,52 +4,16 @@ var nav = document.getElementById('nav');
 var links = document.querySelectorAll("nav li a");
 
 button.addEventListener('click', toggleNav);
-links.forEach( function (element) {
-  element.addEventListener("click", toggleNav)
-} );
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", toggleNav);
+}
 
 function toggleNav() {
   nav.classList.toggle('open');
 }
 
 // Welcome Section Parallax Effect //
-var welcomeSection = document.getElementById('welcome-section');
-var welcomeSectionHeight = welcomeSection.clientHeight;
-var welcomeHeadingContainer = document.querySelector('#welcome-section .welcome-heading-container');
-
-document.addEventListener('scroll', welcome_parallax);
-
-function welcome_parallax() {
-  var windowHeight = window.innerHeight;
-  var scrollY = window.scrollY || window.pageYOffset;
-
-  var welcomeSectionPosition = welcomeSection.getBoundingClientRect().top + scrollY + welcomeSectionHeight;
-
-  if (scrollY <= welcomeSectionPosition) {
-    welcomeHeadingContainer.style.transform = 'translateY(calc(-50% + ' + scrollY / 10 + '%))';
-  }
-
-  return false;
-}
-
-// Fixed Header //
-var header = document.getElementById('header');
-var headerHeight = header.clientHeight;
-var fixed = false;
-
-document.addEventListener('scroll', fixedHeader);
-
-function fixedHeader() {
-  var windowHeight = window.innerHeight;
-  var scrollY = window.scrollY || window.pageYOffset;
-
-  if (scrollY > windowHeight / 5) {
-    fixed = true;
-    header.classList.add('fixed');
-  } else if (scrollY == 0 && fixed) {
-    fixed = false;
-    header.classList.remove('fixed');
-  }
-
-  return false;
+if ( matchMedia("only screen and (min-width: 1024px)").matches ) {
+  var rellax = new Rellax('.rellax');
 }
