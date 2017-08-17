9aebb88d5117b0b06562c811ba10e62e609c0a47
JR Cologne <habichvergessen40@gmail.com>
2017-08-17 18:11:01 +0200

    Gulp fix for Resource Pages, Remove unused code block from Article 6

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('watch', function() {
     gulp.watch('_drafts/*.+(md|markdown|MD)', ['jekyll-watch']);
   }
 
-  gulp.watch(['*.html', '*.php', '_app/php/**/*.php', '_layouts/*.html', '_includes/*.html', 'portfolio/*.html', 'tags/*.html', '_posts/blog/*.html', '!_site/**/*.*'], ['jekyll-watch']);
+  gulp.watch(['*.html', '*.php', '_app/php/**/*.php', '_layouts/*.html', '_includes/*.html', 'portfolio/*.html', 'tags/*.html', '_posts/blog/*.html', 'resources/*.html', '!_site/**/*.*'], ['jekyll-watch']);
 
   gulp.watch('favicon.ico', ['jekyll-watch']);
 });
