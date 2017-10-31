1bf6f1065899d6f2f7843032011b9ac7de842e68
JR Cologne <kontakt@jr-cologne.de>
2017-10-31 12:28:24 +0100

    Release

--- _app/assets/js/article-image.js
+++ _app/assets/js/article-image.js
@@ -1,55 +1,71 @@
 // Article Image Parallax Effect //
 var articleImage = document.getElementById('article-image');
 
-var specificBackgroundPosition = false;
+if (articleImage.getAttribute('data-article-image-effect') != 'false') {
+  var specificBackgroundPosition = false;
 
-if (articleImage.style.backgroundPosition) {
-  specificBackgroundPosition = true;
-  var wantedHorizontalBackgroundPosition = articleImage.style.backgroundPosition.split(' ')[0];
-}
+  if (articleImage.getAttribute('data-bg-pos-x')) {
+    specificBackgroundPosition = true;
+    var wantedHorizontalBackgroundPosition = articleImage.getAttribute('data-bg-pos-x');
+  }
 
-fixArticleImagePosition();
-
-document.addEventListener('scroll', articleImageParallax);
-
-window.addEventListener('resize', function () {
-  disableArticleImageEffects();
   fixArticleImagePosition();
-  articleImageParallax();
-});
 
-function disableArticleImageEffects() {
-  if (window.innerWidth <= 1024) {
-    if (!specificBackgroundPosition) {
-      articleImage.style.backgroundPosition = 'center center';
-    } else {
-      articleImage.style.backgroundPosition = wantedHorizontalBackgroundPosition + ' center';
+  document.addEventListener('scroll', articleImageParallax);
+
+  window.addEventListener('resize', function () {
+    disableArticleImageEffects();
+    fixArticleImagePosition();
+    articleImageParallax();
+  });
+
+  function getWantedHorizontalBackgroundPosition() {
+    switch (wantedHorizontalBackgroundPosition) {
+      case 'start':
+        return (window.innerWidth <= 1024) ? 'left' : articleImage.getBoundingClientRect().left + 'px';
+      default:
+        return wantedHorizontalBackgroundPosition;
     }
   }
-}
 
-function fixArticleImagePosition() {
-  if (window.innerWidth > 1024) {
-    var articleImagePositionTop = articleImage.getBoundingClientRect().top;
-
-    if (!specificBackgroundPosition) {
-      articleImage.style.backgroundPosition = 'center ' + articleImagePositionTop + 'px';
-    } else {
-      articleImage.style.backgroundPosition = wantedHorizontalBackgroundPosition + ' ' + articleImagePositionTop + 'px';
+  function disableArticleImageEffects() {
+    if (window.innerWidth <= 1024) {
+      if (!specificBackgroundPosition) {
+        articleImage.style.backgroundPosition = 'center center';
+      } else {
+        articleImage.style.backgroundPosition = getWantedHorizontalBackgroundPosition() + ' center';
+      }
     }
   }
-}
 
-function articleImageParallax() {
-  if (window.innerWidth > 1024) {
-    var scrollY = window.scrollY || window.pageYOffset;
-    var articleImagePositionTop = articleImage.getBoundingClientRect().top;
-    var parallaxValue = articleImagePositionTop - scrollY / 2;
+  function fixArticleImagePosition() {
+    if (window.innerWidth > 1024) {
+      var articleImagePositionTop = articleImage.getBoundingClientRect().top;
 
-    if (!specificBackgroundPosition) {
-      articleImage.style.backgroundPosition = 'center ' + parallaxValue + 'px';
-    } else {
-      articleImage.style.backgroundPosition = wantedHorizontalBackgroundPosition + ' ' + parallaxValue + 'px';
+      if (!specificBackgroundPosition) {
+        articleImage.style.backgroundPosition = 'center ' + articleImagePositionTop + 'px';
+      } else {
+        articleImage.style.backgroundPosition = getWantedHorizontalBackgroundPosition() + ' ' + articleImagePositionTop + 'px';
+      }
+    } else if (specificBackgroundPosition && window.innerWidth <= 1024) {
+      articleImage.style.backgroundPosition = getWantedHorizontalBackgroundPosition() + ' center';
     }
   }
+
+  function articleImageParallax() {
+    if (window.innerWidth > 1024) {
+      var scrollY = window.scrollY || window.pageYOffset;
+      var articleImagePositionTop = articleImage.getBoundingClientRect().top;
+      var parallaxValue = articleImagePositionTop - scrollY / 2;
+
+      if (!specificBackgroundPosition) {
+        articleImage.style.backgroundPosition = 'center ' + parallaxValue + 'px';
+      } else {
+        articleImage.style.backgroundPosition = getWantedHorizontalBackgroundPosition() + ' ' + parallaxValue + 'px';
+      }
+    }
+  }
+} else {
+  articleImage.style.backgroundAttachment = 'scroll';
+  articleImage.style.backgroundPosition = 'center center';
 }
